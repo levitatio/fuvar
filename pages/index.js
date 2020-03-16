9d74b66a37a210c8fbcf6e599ae901f7b0114a66
Balogh Levente <balogh.levente.istvan@gmail.com>
2020-03-16 19:39:51 +0100

    F-1: Add semistandard and use it

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,43 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 const Home = () => (
-  <div className="container">
+  <div className='container'>
     <Head>
       <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
+      <link rel='icon' href='/favicon.ico' />
     </Head>
 
     <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      <h1 className='title'>
+        Welcome to <a href='https://nextjs.org'>Next.js!</a>
       </h1>
 
-      <p className="description">
+      <p className='description'>
         Get started by editing <code>pages/index.js</code>
       </p>
 
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
+      <div className='grid'>
+        <a href='https://nextjs.org/docs' className='card'>
           <h3>Documentation &rarr;</h3>
           <p>Find in-depth information about Next.js features and API.</p>
         </a>
 
-        <a href="https://nextjs.org/learn" className="card">
+        <a href='https://nextjs.org/learn' className='card'>
           <h3>Learn &rarr;</h3>
           <p>Learn about Next.js in an interactive course with quizzes!</p>
         </a>
 
         <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
+          href='https://github.com/zeit/next.js/tree/master/examples'
+          className='card'
         >
           <h3>Examples &rarr;</h3>
           <p>Discover and deploy boilerplate example Next.js projects.</p>
         </a>
 
         <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
+          href='https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+          className='card'
         >
           <h3>Deploy &rarr;</h3>
           <p>
@@ -49,11 +49,11 @@ const Home = () => (
 
     <footer>
       <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
+        href='https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+        target='_blank'
+        rel='noopener noreferrer'
       >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
+        Powered by <img src='/zeit.svg' alt='ZEIT Logo' />
       </a>
     </footer>
 
@@ -182,7 +182,8 @@ const Home = () => (
           flex-direction: column;
         }
       }
-    `}</style>
+    `}
+    </style>
 
     <style jsx global>{`
       html,
@@ -196,8 +197,9 @@ const Home = () => (
       * {
         box-sizing: border-box;
       }
-    `}</style>
+    `}
+    </style>
   </div>
-)
+);
 
-export default Home
+export default Home;
