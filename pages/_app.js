9797c772c17d81b1c2e84e9d3aae1dd0c96035ad
Balogh Levente <balogh.levente.istvan@gmail.com>
2020-03-16 19:39:51 +0100

    F-1: Add semistandard and use it

--- pages/_app.js
+++ pages/_app.js
@@ -2,22 +2,21 @@ import App from 'next/app';
 import Head from 'next/head';
 import Header from '../partials/Header';
 
-function FuvarApp({ Component, pageProps }) {
-    return (
-        <>
-            <Head>
-                <title>My first Next</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <Header {...pageProps} />
-            <Component {...pageProps} />
-        </>
-    );
-
+function FuvarApp ({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>My first Next</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Header {...pageProps} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export async function getServerSideProps(appContext) {
-    const appProps = await App.getServerSideProps(appContext); 
-    return { ...appProps };
+export async function getServerSideProps (appContext) {
+  const appProps = await App.getServerSideProps(appContext);
+  return { ...appProps };
 }
 export default FuvarApp;
--- partials/Header.jsx
+++ partials/Header.jsx
@@ -1,25 +1,22 @@
 import Button from '../components/button/Button';
 
-function Header(user) {
+function Header (user) {
+  return (
+    <>
+      <div className='logo' />
+      <div className='businessOptions'>
+        <Button text='Így működik' />
+        <Button text='Ajánlatkérés' />
+        <Button text='Környezetünkért' />
+      </div>
+      <div className='userOptions'>
+        <Button text='Így működik' />
+        <Button text='Ajánlatkérés' />
+        <Button text='Környezetünkért' />
 
-    return (
-        <>
-            <div className="logo">
-
-            </div>
-            <div className="businessOptions">
-                <Button text="Így működik" />
-                <Button text="Ajánlatkérés" />
-                <Button text="Környezetünkért" />
-            </div>
-            <div className="userOptions">
-                <Button text="Így működik" />
-                <Button text="Ajánlatkérés" />
-                <Button text="Környezetünkért" />
-
-            </div>
-        </>
-    );
+      </div>
+    </>
+  );
 }
 
 export default Header;
