5cc8e58c184f150a8039978c6f0bcb8a4bca2965
Balogh Levente <balogh.levente.istvan@gmail.com>
2020-03-17 22:27:55 +0100

    F-5: Add logo to the header

--- partials/Header.jsx
+++ partials/Header.jsx
@@ -4,19 +4,21 @@ import { withTranslation } from '../services/i18n';
 
 function Header ({ t }, user) {
   return (
-    <>
-      <div className='logo' />
-      <div className='businessOptions'>
+    <div className='container'>
+      <a class='headerBar__logo' href='/' title='Fuvar.hu'>
+        <img src='assets/logo.svg' className='Logo' />
+      </a>
+      <div className='navBar u-floatLeft u-desktopBigOnly'>
         <Button text={t('how-it-works')} />
         <Button text={t('send-me-a-quote')} />
         <Button text={t('for-our-environment')} />
       </div>
-      <div className='userOptions'>
-        <Button text={t('how-it-works')} />
-        <Button text={t('send-me-a-quote')} />
-        <Button text={t('for-our-environment')} />
+      <div className='navBar navBar--secondary u-right-10 u-desktopBigOnly'>
+        <Button text={t('login')} />
+        <Button text={t('registration')} />
+        <Button text={t('help')} />
       </div>
-    </>
+    </div>
   );
 }
 
