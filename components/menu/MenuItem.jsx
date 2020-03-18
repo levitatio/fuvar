4e9f69716a64a8ad58fcc042e00dea41f0df1958
Balogh Levente <balogh.levente.istvan@gmail.com>
2020-03-18 15:11:21 +0100

    F-7: Add Link tag to MenuItem

--- components/menu/MenuItem.jsx
+++ components/menu/MenuItem.jsx
@@ -1,12 +1,15 @@
+import Link from 'next/link';
 import PropTypes from 'prop-types';
 
 function MenuItem (props) {
   return (
-    <a href={props.href} className={props.class} title={props.title}>
-      {props.icon && <img src={props.icon} />}
-      <span>{props.text}</span>
-      {props.children}
-    </a>
+    <Link href={props.href}>
+      <a className={props.class} title={props.title}>
+        {props.icon && <img src={props.icon} />}
+        <span>{props.text}</span>
+        {props.children}
+      </a>
+    </Link>
   );
 }
 
