ff5f38eb85cdcb554763be0bed295a424d369f7b
Balogh Levente <balogh.levente.istvan@gmail.com>
2020-03-18 16:16:43 +0100

    F-9: Add Footer and refactor Header
    F-9: Add LanguageButton and set language options

--- partials/Header.jsx
+++ partials/Header.jsx
@@ -1,46 +1,46 @@
-import Button from '../components/button/Button';
 import PropTypes from 'prop-types';
 import { withTranslation } from '../services/i18n';
 import MenuItem from '../components/menu/MenuItem';
 
 function Header ({ t }, user) {
   return (
-    <div className='container'>
-      <a className='headerBar__logo' href='/' title='Fuvar.hu'>
-        <img src='assets/logo.svg' className='Logo' />
-      </a>
-      <nav className='navBar u-floatLeft u-desktopBigOnly'>
-        <ul>
-          <li><MenuItem href='/' className='item' icon='assets/help.svg' text={t('how-it-works')} /></li>
-          <li><MenuItem href='/' className='item' icon='assets/add-box.svg' text={t('send-me-a-quote')} /></li>
-          <li><MenuItem href='/' className='item' icon='assets/tree.svg' text={t('for-our-environment')} /></li>
-        </ul>
-      </nav>
-      <nav className='navBar navBar--secondary u-right-10 u-desktopBigOnly'>
-        <ul>
-          <li><MenuItem href='/' className='item' text={t('login')} /></li>
-          <li><MenuItem href='/' className='item' text={t('registration')} /></li>
-          <li><MenuItem href='/' className='item' text={t('help')} /></li>
-        </ul>
-      </nav>
-      <nav className='mobileNavButton u-desktopBigHidden js-mobileNavButton'>
-        <svg className='icn icn--24 icn--primary js-openMobileNav' title='Navigáció'>
-
-          <svg viewBox='0 0 24 24' id='icn-menu'>
-            <text fontSize='45px' x='0' y='24'>&#8801;</text>
+    <header className='headerBar u-clearfix js-headerBar'>
+      <div className='container'>
+        <a className='headerBar__logo' href='/' title='Fuvar.hu'>
+          <img src='assets/logo.svg' className='Logo' />
+        </a>
+        <nav className='navBar u-floatLeft u-desktopBigOnly'>
+          <ul>
+            <li><MenuItem href='/' className='item' icon='assets/help.svg' text={t('így-működik')} /></li>
+            <li><MenuItem href='/' className='js-newShipmentButton' icon='assets/add-box.svg' text={t('ajánlatkérés')} /></li>
+            <li><MenuItem href='/' className='item' icon='assets/tree.svg' text={t('környezetünkért')} /></li>
+          </ul>
+        </nav>
+        <nav className='navBar navBar--secondary u-right-10 u-desktopBigOnly'>
+          <ul>
+            <li><MenuItem href='/' className='js-openAuthModal' text={t('bejelentkezés')} /></li>
+            <li><MenuItem href='/' className='item' text={t('regisztráció')} /></li>
+            <li><MenuItem href='/' className='item' text={t('segítség')} /></li>
+          </ul>
+        </nav>
+        <nav className='mobileNavButton u-desktopBigHidden js-mobileNavButton'>
+          <svg className='icn icn--24 icn--primary js-openMobileNav' title='Navigáció'>
+            <svg viewBox='0 0 24 24' id='icn-menu'>
+              <text fontSize='45px' x='0' y='24'>&#8801;</text>
+            </svg>
           </svg>
-        </svg>
-      </nav>
-    </div>
+        </nav>
+      </div>
+    </header>
   );
 }
 
-Header.getInitialProps = async () => ({
-  namespacesRequired: ['common']
+Header.getServerSideProps = async () => ({
+  namespacesRequired: ['header']
 });
 
 Header.propTypes = {
   t: PropTypes.func.isRequired
 };
 
-export default withTranslation('common')(Header);
+export default withTranslation('header')(Header);
--- services/i18n.js
+++ services/i18n.js
@@ -2,7 +2,11 @@ const NextI18Next = require('next-i18next').default;
 
 const NextI18NextInstance = new NextI18Next({
   defaultLanguage: 'hu',
-  otherLanguages: ['en', 'hr']
+  otherLanguages: ['en', 'hr'],
+  localeSubPath: {
+    en: 'en',
+    hr: 'hr'
+  }
 });
 
 module.exports = NextI18NextInstance;
